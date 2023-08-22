160a8845c1f685b0891483683e515fcd9024c25e
Elias Santistevan <elias.santistevan@sparkfun.com>
2023-08-22 11:47:20 -0600

    Moves all comments to the header, renames example10 to 6

--- src/sfe_opt4048.cpp
+++ src/sfe_opt4048.cpp
@@ -1,8 +1,6 @@
 #include "sfe_opt4048.h"
 #include "OPT4048_Registers.h"
 
-/// @brief Sets the struct that interfaces with STMicroelectronic's C Library.
-/// @return true on successful execution.
 bool QwOpt4048::init(void)
 {
     if (!_sfeBus->ping(_i2cAddress))
@@ -14,9 +12,6 @@ bool QwOpt4048::init(void)
     return true;
 }
 
-/// @brief Checks that the bus is connected with the OPT4048 by checking
-/// it's unique ID.
-/// @return True on successful execution.
 bool QwOpt4048::isConnected()
 {
     if (getDeviceID() != OPT4048_DEVICE_ID)
@@ -25,8 +20,6 @@ bool QwOpt4048::isConnected()
         return true;
 }
 
-/// @brief Retrieves the the device's ID: 0x24 for the OPT4048.
-/// @return Returns the unique ID.
 uint16_t QwOpt4048::getDeviceID()
 {
 
@@ -49,43 +42,27 @@ uint16_t QwOpt4048::getDeviceID()
     return uniqueId;
 }
 
-/// @brief Sets the pointer to the data bus for read and writes.
-/// @param theBus This parameter sets the the I2C hardware bus.
-/// @param i2cAddress The I2C address for the device.
 void QwOpt4048::setCommunicationBus(sfe_OPT4048::QwDeviceBus &theBus, uint8_t i2cAddress)
 {
     _sfeBus = &theBus;
     _i2cAddress = i2cAddress;
 }
 
-/// @brief Sets the pointer to the data bus for read and writes.
-/// @param theBus This parameter sets the hardware bus.
 void QwOpt4048::setCommunicationBus(sfe_OPT4048::QwDeviceBus &theBus)
 {
     _sfeBus = &theBus;
 }
 
-/// @brief Writes to the data to the given register using the hardware data bus.
-/// @param  offset The register to write to.
-/// @param  data The data to write to the register.
-/// @param  length The number of writes
-/// @return The successful (0) or unsuccessful (-1) write to the given register.
 int32_t QwOpt4048::writeRegisterRegion(uint8_t offset, uint8_t *data, uint16_t length)
 {
     return _sfeBus->writeRegisterRegion(_i2cAddress, offset, data, length);
 }
 
-/// @brief Reads data from the specified register using the set data bas.
-/// @param  offset The register to read from.
-/// @param  data The pointer to the value to store the value.
-/// @param  length The number of reads
-/// @return The successful (0) or unsuccessful (-1) read of the given register.
 int32_t QwOpt4048::readRegisterRegion(uint8_t offset, uint8_t *data, uint16_t length)
 {
     return _sfeBus->readRegisterRegion(_i2cAddress, offset, data, length);
 }
 
-/// @brief Sets the minimum of settings to get the board running.
 void QwOpt4048::setBasicSetup()
 {
     setRange(RANGE_36LUX);
@@ -93,17 +70,6 @@ void QwOpt4048::setBasicSetup()
     setOperationMode(OPERATION_MODE_CONTINUOUS);
 }
 
-/// @brief Sets the OPT4048's effective sensing range which will effect its resolution.
-/// @param range The range to set the device to.
-///   RANGE_2KLUX2,
-///   RANGE_4KLUX5,
-///   RANGE_9LUX,
-///   RANGE_18LUX,
-///   RANGE_36LUX,
-///   RANGE_72LUX,
-///   RANGE_144LUX,
-///   RANGE_AUTO
-/// @return True on successful execution.
 bool QwOpt4048::setRange(opt4048_range_t range)
 {
     uint8_t buff[2];
@@ -131,8 +97,6 @@ bool QwOpt4048::setRange(opt4048_range_t range)
     return true;
 }
 
-/// @brief Retrieves the light range in lux of the OPT4048.
-/// @return The range of lux able to be measured.
 opt4048_range_t QwOpt4048::getRange()
 {
     uint8_t buff[2];
@@ -146,22 +110,6 @@ opt4048_range_t QwOpt4048::getRange()
     return (opt4048_range_t)controlReg.range;
 }
 
-/// @brief Sets the OPT4048's conversion time which will effect its resolution. Longer conversion time
-/// will result in higher resolution.
-/// @param time The conversion time to set the device to. Possible values:
-///   CONVERSION_TIME_600US,
-///   CONVERSION_TIME_1MS,
-///   CONVERSION_TIME_1MS8,
-///   CONVERSION_TIME_3MS4,
-///   CONVERSION_TIME_6MS5,
-///   CONVERSION_TIME_12MS7,
-///   CONVERSION_TIME_25MS,
-///   CONVERSION_TIME_50MS,
-///   CONVERSION_TIME_100MS,
-///   CONVERSION_TIME_200MS,
-///   CONVERSION_TIME_400MS,
-///   CONVERSION_TIME_800MS
-/// @return True on successful execution.
 bool QwOpt4048::setConversionTime(opt4048_conversion_time_t time)
 {
     uint8_t buff[2];
@@ -189,8 +137,6 @@ bool QwOpt4048::setConversionTime(opt4048_conversion_time_t time)
     return true;
 }
 
-/// @brief Retrieves the conversion time used for the ADC.
-/// @return The OPT4048 conversion time.
 opt4048_conversion_time_t QwOpt4048::getConversionTime()
 {
     uint8_t buff[2];
@@ -204,9 +150,6 @@ opt4048_conversion_time_t QwOpt4048::getConversionTime()
     return (opt4048_conversion_time_t)controlReg.conversion_time;
 }
 
-/// @brief Enable the quick wake up feature of the OPT4048.
-/// @param enable True to enable, false to disable.
-/// @return True on successful execution.
 bool QwOpt4048::enableQwake(bool enable)
 {
     uint8_t buff[2];
@@ -234,8 +177,6 @@ bool QwOpt4048::enableQwake(bool enable)
     return true;
 }
 
-/// @brief Checks the quick wake bit.
-/// @return True the quick wake bit is set.
 bool QwOpt4048::getQwake()
 {
     uint8_t buff[2];
@@ -252,13 +193,6 @@ bool QwOpt4048::getQwake()
     return true;
 }
 
-/// @brief Sets the OPT4048's operation mode.
-/// @param mode The mode to set the device to. Possible Values:
-///   OPERATION_MODE_POWER_DOWN,
-///   OPERATION_MODE_AUTO_ONE_SHOT,
-///   OPERATION_MODE_ONE_SHOT,
-///   OPERATION_MODE_CONTINUOUS
-/// @return
 bool QwOpt4048::setOperationMode(opt4048_operation_mode_t mode)
 {
     uint8_t buff[2];
@@ -286,8 +220,6 @@ bool QwOpt4048::setOperationMode(opt4048_operation_mode_t mode)
     return true;
 }
 
-/// @brief Retrieves the set operation mode.
-/// @return The OPT4048 conversion time.
 opt4048_operation_mode_t QwOpt4048::getOperationMode()
 {
     uint8_t buff[2];
@@ -301,9 +233,6 @@ opt4048_operation_mode_t QwOpt4048::getOperationMode()
     return (opt4048_operation_mode_t)controlReg.op_mode;
 }
 
-/// @brief Changes the behavior of the interrupt from pin to latch.
-/// @param enable True to enable, false to disable.
-/// @return True on successful execution.
 bool QwOpt4048::enableIntLatch(bool enable)
 {
     uint8_t buff[2];
@@ -331,8 +260,6 @@ bool QwOpt4048::enableIntLatch(bool enable)
     return true;
 }
 
-/// @brief Checks if the interrupt is set to pulse or latch.
-/// @return True if set to latch, false if not.
 bool QwOpt4048::getIntLatch()
 {
     uint8_t buff[2];
@@ -349,9 +276,6 @@ bool QwOpt4048::getIntLatch()
     return false;
 }
 
-/// @brief Sets the OPT4048's interrupt polarity.
-/// @param enable True to enable, false to disable.
-/// @return True on successful execution.
 bool QwOpt4048::enableIntActiveHigh(bool enable)
 {
     uint8_t buff[2];
@@ -379,9 +303,6 @@ bool QwOpt4048::enableIntActiveHigh(bool enable)
     return true;
 }
 
-/// @brief Sets the OPT4048's interrupt polarity.
-/// @param enable True to enable, false to disable.
-/// @return True on successful execution.
 bool QwOpt4048::getIntActiveHigh()
 {
     uint8_t buff[2];
@@ -398,10 +319,6 @@ bool QwOpt4048::getIntActiveHigh()
     return true;
 }
 
-/// @brief Changes the behavior of the interrupt pin to be an INPUT to trigger
-/// single shot.
-/// @param enable True to enable, false to disable.
-/// @return True on successful execution.
 bool QwOpt4048::enableIntInput(bool enable)
 {
     uint8_t buff[2];
@@ -429,8 +346,6 @@ bool QwOpt4048::enableIntInput(bool enable)
     return true;
 }
 
-/// @brief Gets the interrupt input bit
-/// @return True if the interrupt is set to INPUT.
 bool QwOpt4048::getIntInputEnable()
 {
     uint8_t buff[2];
@@ -447,9 +362,6 @@ bool QwOpt4048::getIntInputEnable()
     return true;
 }
 
-/// @brief Changes the behavior interrupt mechanism after the end of conversion
-/// @param mechanism The mechanism to set
-/// @return True on successful execution.
 bool QwOpt4048::setIntMechanism(opt4048_mechanism_t mechanism)
 {
     uint8_t buff[2];
@@ -477,8 +389,6 @@ bool QwOpt4048::setIntMechanism(opt4048_mechanism_t mechanism)
     return true;
 }
 
-/// @brief Gets the interrupt mechanism for the OPT4048
-/// @return Returns the enabled mechanism.
 opt4048_mechanism_t QwOpt4048::getIntMechanism()
 {
     uint8_t buff[2];
@@ -492,8 +402,6 @@ opt4048_mechanism_t QwOpt4048::getIntMechanism()
     return ((opt4048_mechanism_t)intReg.int_cfg);
 }
 
-/// @brief Retrieves the flag register
-/// @return The contents of the flag register
 opt4048_reg_flags_t QwOpt4048::getAllFlags()
 {
     uint8_t buff[2];
@@ -507,8 +415,6 @@ opt4048_reg_flags_t QwOpt4048::getAllFlags()
     return flagReg;
 }
 
-/// @brief Checks the overload flag bit.
-/// @return True if the overload flag bit is set, false otherwise
 bool QwOpt4048::getOverloadFlag()
 {
     opt4048_reg_flags_t flagReg;
@@ -520,8 +426,6 @@ bool QwOpt4048::getOverloadFlag()
     return true;
 }
 
-/// @brief Checks the conversion ready flag bit.
-/// @return True if that flag bit is set, false otherwise
 bool QwOpt4048::getConvReadyFlag()
 {
     opt4048_reg_flags_t flagReg;
@@ -533,8 +437,6 @@ bool QwOpt4048::getConvReadyFlag()
     return true;
 }
 
-/// @brief Checks the too bright flag bit.
-/// @return True if that flag bit is set, false otherwise
 bool QwOpt4048::getTooBrightFlag()
 {
     opt4048_reg_flags_t flagReg;
@@ -546,8 +448,6 @@ bool QwOpt4048::getTooBrightFlag()
     return true;
 }
 
-/// @brief Checks the too dim flag bit.
-/// @return True if that flag bit is set, false otherwise
 bool QwOpt4048::getTooDimFlag()
 {
     opt4048_reg_flags_t flagReg;
@@ -559,13 +459,6 @@ bool QwOpt4048::getTooDimFlag()
     return true;
 }
 
-/// @brief Sets the number of faults (light values over or under) before an interrupt is triggered.
-/// @param count The number of faults to trigger an interrupt
-///   FAULT_COUNT_1,
-///   FAULT_COUNT_2,
-///   FAULT_COUNT_4,
-///   FAULT_COUNT_8
-/// @return True on successful execution.
 bool QwOpt4048::setFaultCount(opt4048_fault_count_t count)
 {
     uint8_t buff[2];
@@ -593,8 +486,6 @@ bool QwOpt4048::setFaultCount(opt4048_fault_count_t count)
     return true;
 }
 
-/// @brief Retrieves the number of faults (light values over or under) before an interrupt is triggered.
-/// @return The fault count.
 opt4048_fault_count_t QwOpt4048::getFaultCount()
 {
     uint8_t buff[2];
@@ -609,13 +500,6 @@ opt4048_fault_count_t QwOpt4048::getFaultCount()
     return ((opt4048_fault_count_t)controlReg.fault_count);
 }
 
-/// @brief Sets the channel for threshold logic
-/// @param channel The channel to set the threshold logic to.
-///   THRESH_CHANNEL_CH0,
-///   THRESH_CHANNEL_CH1,
-///   THRESH_CHANNEL_CH2,
-///   THRESH_CHANNEL_CH3
-/// @return True on successful execution.
 bool QwOpt4048::setThresholdChannel(opt4048_threshold_channel_t channel)
 {
     uint8_t buff[2];
@@ -643,8 +527,6 @@ bool QwOpt4048::setThresholdChannel(opt4048_threshold_channel_t channel)
     return true;
 }
 
-/// @brief Retrives the threshold channel.
-/// @return True on successful execution.
 opt4048_threshold_channel_t QwOpt4048::getThresholdChannel()
 {
     uint8_t buff[2];
@@ -658,9 +540,6 @@ opt4048_threshold_channel_t QwOpt4048::getThresholdChannel()
     return ((opt4048_threshold_channel_t)intReg.threshold_ch_sel);
 }
 
-/// @brief Sets the Lux High Value threshold.
-/// @param thresh The value in float
-/// @return Returns the high threshold.
 bool QwOpt4048::setThresholdHigh(float thresh)
 {
     if (thresh < 2.15 || thresh > 144000)
@@ -678,8 +557,6 @@ bool QwOpt4048::setThresholdHigh(float thresh)
     return true;
 }
 
-/// @brief Retrieves the Lux High Value threshold.
-/// @return Returns the high threshold.
 uint16_t QwOpt4048::getThresholdHigh()
 {
     uint8_t buff[2];
@@ -696,9 +573,6 @@ uint16_t QwOpt4048::getThresholdHigh()
     return thresholdHigh;
 }
 
-/// @brief Sets the Lux Low Value threshold.
-/// @param thresh The value in float
-/// @return Returns the high threshold.
 bool QwOpt4048::setThresholdLow(float thresh)
 {
     if (thresh < 2.15 || thresh > 144000)
@@ -716,8 +590,6 @@ bool QwOpt4048::setThresholdLow(float thresh)
     return true;
 }
 
-/// @brief Retrieves the Lux Low Value threshold.
-/// @return Returns the low threshold.
 uint16_t QwOpt4048::getThresholdLow()
 {
     uint8_t buff[2];
@@ -734,9 +606,6 @@ uint16_t QwOpt4048::getThresholdLow()
     return thresholdLow;
 }
 
-/// @brief Enable register auto increment .
-/// @param enable True to enable, false to disable.
-/// @return True on successful execution.
 bool QwOpt4048::enableI2CBurst(bool enable)
 {
     uint8_t buff[2];
@@ -764,8 +633,6 @@ bool QwOpt4048::enableI2CBurst(bool enable)
     return true;
 }
 
-/// @brief Retrieves the I2C burst bit.
-/// @return True if I2C burst is enabled, false otherwise.
 bool QwOpt4048::getI2CBurst()
 {
     uint8_t buff[2];
@@ -782,9 +649,6 @@ bool QwOpt4048::getI2CBurst()
     return true;
 }
 
-/// @brief Enable CRC for ADC calues
-/// @param enable True to enable, false to disable.
-/// @return True on successful execution.
 void QwOpt4048::enableCRC(bool enable)
 {
     if (enable)
@@ -793,8 +657,6 @@ void QwOpt4048::enableCRC(bool enable)
         crcEnabled = false;
 }
 
-/// @brief Reads Channel Zero (Red)
-/// @return Returns the ADC value of Channel Zero
 uint32_t QwOpt4048::getADCCh0()
 {
     uint8_t buff[4];
@@ -818,8 +680,6 @@ uint32_t QwOpt4048::getADCCh0()
     return adcCode;
 }
 
-/// @brief Reads Channel One (Green)
-/// @return Returns the ADC value of Channel One
 uint32_t QwOpt4048::getADCCh1()
 {
 
@@ -843,8 +703,6 @@ uint32_t QwOpt4048::getADCCh1()
     return adcCode;
 }
 
-/// @brief Reads Channel Two (Blue)
-/// @return Returns the ADC value of Channel Two
 uint32_t QwOpt4048::getADCCh2()
 {
 
@@ -869,8 +727,6 @@ uint32_t QwOpt4048::getADCCh2()
     return adcCode;
 }
 
-/// @brief Reads Channel Three (White)
-/// @return Returns the ADC value of Channel Three
 uint32_t QwOpt4048::getADCCh3()
 {
 
@@ -895,8 +751,6 @@ uint32_t QwOpt4048::getADCCh3()
     return adcCode;
 }
 
-/// @brief Retrieves all ADC values for all channels: Red, Green, Blue, and White.
-/// @return Returns the ADC values of the channels.
 sfe_color_t QwOpt4048::getAllADC()
 {
 
@@ -910,10 +764,6 @@ sfe_color_t QwOpt4048::getAllADC()
     return color;
 }
 
-/// @brief Retrieves all ADC values for all channels: Red, Green, Blue, and White, as well as the sample counter, and
-///        the CRC value.
-/// @param color Pointer to the color struct to be populated with the channels values.
-/// @return Returns true on successful execution, false otherwise.
 bool QwOpt4048::getAllChannelData(sfe_color_t *color)
 {
     int32_t retVal;
@@ -995,12 +845,6 @@ bool QwOpt4048::getAllChannelData(sfe_color_t *color)
     return true;
 }
 
-/// @brief  Calculates the CRC for the OPT4048. Note that the OPT4048 does this already
-///         but this is a way to double check the value.
-/// @param mantissa The mantissa value of the ADC
-/// @param exponent The exponent value of the ADC
-/// @param crc The CRC value of the ADC
-/// @return Returns the calculated CRC value.
 uint8_t QwOpt4048::calculateCRC(uint32_t mantissa, uint8_t expon, uint8_t crc)
 {
 
@@ -1030,8 +874,6 @@ uint8_t QwOpt4048::calculateCRC(uint32_t mantissa, uint8_t expon, uint8_t crc)
     return 0;
 }
 
-/// @brief Retrieves the Lux value.
-/// @return Returns the Lux value of the sensor
 uint32_t QwOpt4048::getLux()
 {
     uint32_t adcCh1;
@@ -1043,8 +885,6 @@ uint32_t QwOpt4048::getLux()
     return lux;
 }
 
-/// @brief  Retrieves the CIE X value of the sensor.
-/// @return Returns the CIE X value of the sensor
 double QwOpt4048::getCIEx()
 {
     double x = 0;
@@ -1067,8 +907,6 @@ double QwOpt4048::getCIEx()
     return CIEx;
 }
 
-/// @brief Retrieves the CIE Y value of the sensor.
-/// @return Returns the CIE Y value of the sensor
 double QwOpt4048::getCIEy()
 {
     double x = 0;
@@ -1091,8 +929,6 @@ double QwOpt4048::getCIEy()
     return CIEy;
 }
 
-/// @brief Retrieves the Correlated Color Temperature (CCT) of the sensor.
-/// @return Returns the CCT of the sensor in Kelvin
 double QwOpt4048::getCCT()
 {
     double CIEx;
--- src/sfe_opt4048.h
+++ src/sfe_opt4048.h
@@ -81,79 +81,275 @@ class QwOpt4048
   public:
     QwOpt4048() : _i2cAddress{0} {};
 
+    /// @brief Sets the struct that interfaces with STMicroelectronic's C Library.
+    /// @return true on successful execution.
     bool init();
+
+    /// @brief Checks that the bus is connected with the OPT4048 by checking
+    /// it's unique ID.
+    /// @return True on successful execution.
     bool isConnected();
+
+    /// @brief Retrieves the the device's ID: 0x24 for the OPT4048.
+    /// @return Returns the unique ID.
     uint16_t getDeviceID();
 
+    /// @brief Sets the pointer to the data bus for read and writes.
+    /// @param theBus This parameter sets the the I2C hardware bus.
+    /// @param i2cAddress The I2C address for the device.
     void setCommunicationBus(sfe_OPT4048::QwDeviceBus &theBus, uint8_t i2cAddress);
+
+    /// @brief Sets the pointer to the data bus for read and writes.
+    /// @param theBus This parameter sets the hardware bus.
     void setCommunicationBus(sfe_OPT4048::QwDeviceBus &theBus);
 
+    /// @brief Writes to the data to the given register using the hardware data bus.
+    /// @param  offset The register to write to.
+    /// @param  data The data to write to the register.
+    /// @param  length The number of writes
+    /// @return The successful (0) or unsuccessful (-1) write to the given register.
     int32_t writeRegisterRegion(uint8_t offset, uint8_t *data, uint16_t length = 2);
+
+    /// @brief Reads data from the specified register using the set data bas.
+    /// @param  offset The register to read from.
+    /// @param  data The pointer to the value to store the value.
+    /// @param  length The number of reads
+    /// @return The successful (0) or unsuccessful (-1) read of the given register.
     int32_t readRegisterRegion(uint8_t offset, uint8_t *data, uint16_t numBytes = 2);
 
     ///////////////////////////////////////////////////////////////////Device Settings
 
+    /// @brief Sets the minimum of settings to get the board running.
     void setBasicSetup();
 
+    /// @brief Sets the OPT4048's effective sensing range which will effect its resolution.
+    /// @param range The range to set the device to.
+    /// @return True on successful execution.
     bool enableQwake(bool enable = true);
+    
+    /// @brief Retrieves the light range in lux of the OPT4048.
+    /// @return The range of lux able to be measured.
     bool getQwake();
 
+    /// @brief Sets the OPT4048's effective sensing range which will effect its resolution.
+    /// @param range The range to set the device to.
+    ///   RANGE_2KLUX2,
+    ///   RANGE_4KLUX5,
+    ///   RANGE_9LUX,
+    ///   RANGE_18LUX,
+    ///   RANGE_36LUX,
+    ///   RANGE_72LUX,
+    ///   RANGE_144LUX,
+    ///   RANGE_AUTO
+    /// @return True on successful execution.
     bool setRange(opt4048_range_t range);
+    
+    /// @brief Retrieves the light range in lux of the OPT4048.
+    /// @return The range of lux able to be measured.
     opt4048_range_t getRange();
 
+    /// @brief Sets the OPT4048's conversion time which will effect its resolution. Longer conversion time
+    /// will result in higher resolution.
+    /// @param time The conversion time to set the device to. Possible values:
+    ///   CONVERSION_TIME_600US,
+    ///   CONVERSION_TIME_1MS,
+    ///   CONVERSION_TIME_1MS8,
+    ///   CONVERSION_TIME_3MS4,
+    ///   CONVERSION_TIME_6MS5,
+    ///   CONVERSION_TIME_12MS7,
+    ///   CONVERSION_TIME_25MS,
+    ///   CONVERSION_TIME_50MS,
+    ///   CONVERSION_TIME_100MS,
+    ///   CONVERSION_TIME_200MS,
+    ///   CONVERSION_TIME_400MS,
+    ///   CONVERSION_TIME_800MS
+    /// @return True on successful execution.
     bool setConversionTime(opt4048_conversion_time_t time);
+
+    /// @brief Retrieves the conversion time used for the ADC.
+    /// @return The OPT4048 conversion time.
     opt4048_conversion_time_t getConversionTime();
 
+
+    /// @brief Sets the OPT4048's operation mode.
+    /// @param mode The mode to set the device to. Possible Values:
+    ///   OPERATION_MODE_POWER_DOWN,
+    ///   OPERATION_MODE_AUTO_ONE_SHOT,
+    ///   OPERATION_MODE_ONE_SHOT,
+    ///   OPERATION_MODE_CONTINUOUS
+    /// @return
     bool setOperationMode(opt4048_operation_mode_t mode);
+
+    /// @brief Retrieves the set operation mode.
+    /// @return The OPT4048 conversion time.
     opt4048_operation_mode_t getOperationMode();
 
+    /// @brief Changes the behavior of the interrupt from pin to latch.
+    /// @param enable True to enable, false to disable.
+    /// @return True on successful execution.
     bool enableIntLatch(bool enable = true);
+
+    /// @brief Checks if the interrupt is set to pulse or latch.
+    /// @return True if set to latch, false if not.
     bool getIntLatch();
 
+    /// @brief Sets the OPT4048's interrupt polarity.
+    /// @param enable True to enable, false to disable.
+    /// @return True on successful execution.
     bool enableIntActiveHigh(bool enable = true);
+
+    /// @brief Sets the OPT4048's interrupt polarity.
+    /// @param enable True to enable, false to disable.
+    /// @return True on successful execution.
     bool getIntActiveHigh();
 
+    /// @brief Sets the number of faults (light values over or under) before an interrupt is triggered.
+    /// @param count The number of faults to trigger an interrupt
+    ///   FAULT_COUNT_1,
+    ///   FAULT_COUNT_2,
+    ///   FAULT_COUNT_4,
+    ///   FAULT_COUNT_8
+    /// @return True on successful execution.
     bool setFaultCount(opt4048_fault_count_t count);
+
+    /// @brief Retrieves the number of faults (light values over or under) before 
+    /// an interrupt is triggered.
+    /// @return The fault count.
     opt4048_fault_count_t getFaultCount();
 
+    /// @brief Sets the channel for threshold logic
+    /// @param channel The channel to set the threshold logic to.
+    ///   THRESH_CHANNEL_CH0,
+    ///   THRESH_CHANNEL_CH1,
+    ///   THRESH_CHANNEL_CH2,
+    ///   THRESH_CHANNEL_CH3
+    /// @return True on successful execution.
     bool setThresholdChannel(opt4048_threshold_channel_t channel);
+
+    /// @brief Retrives the threshold channel.
+    /// @return THe selected channel. 
     opt4048_threshold_channel_t getThresholdChannel();
 
+    /// @brief Sets the Lux High Value threshold.
+    /// @param thresh The value in float
+    /// @return Returns the high threshold.
     bool setThresholdHigh(float thresh);
+    
+    /// @brief Retrieves the Lux High Value threshold.
+    /// @return Returns the high threshold.
     uint16_t getThresholdHigh();
+
+    /// @brief Sets the Lux Low Value threshold.
+    /// @param thresh The value in float
+    /// @return Returns the high threshold.
     bool setThresholdLow(float thresh);
+
+    /// @brief Retrieves the Lux Low Value threshold.
+    /// @return Returns the low threshold.
     uint16_t getThresholdLow();
 
+    /// @brief Enable CRC for ADC calues
+    /// @param enable True to enable, false to disable.
+    /// @return True on successful execution.
     void enableCRC(bool enable = true);
 
     ///////////////////////////////////////////////////////////////////Interrupt Settings
+    /// @brief Changes the behavior of the interrupt pin to be an INPUT to trigger single shot.
+    /// @param enable True to enable, false to disable.
+    /// @return True on successful execution.
     bool enableIntInput(bool enable = true);
+
+    /// @brief Gets the interrupt input bit
+    /// @return True if the interrupt is set to INPUT.
     bool getIntInputEnable();
 
+    /// @brief Changes the behavior interrupt mechanism after the end of conversion
+    /// @param mechanism The mechanism to set
+    /// @return True on successful execution.
     bool setIntMechanism(opt4048_mechanism_t mechanism);
+
+    /// @brief Gets the interrupt mechanism for the OPT4048
+    /// @return Returns the enabled mechanism.
     opt4048_mechanism_t getIntMechanism();
 
+    /// @brief Enable register auto increment .
+    /// @param enable True to enable, false to disable.
+    /// @return True on successful execution.
     bool enableI2CBurst(bool enable = true);
+
+    /// @brief Retrieves the I2C burst bit.
+    /// @return True if I2C burst is enabled, false otherwise.
     bool getI2CBurst();
 
+    /// @brief Retrieves the flag register
+    /// @return The contents of the flag register
     opt4048_reg_flags_t getAllFlags();
-    bool getOverloadFlag();
-    bool getConvReadyFlag();
-    bool getTooBrightFlag();
-    bool getTooDimFlag();
 
+    /// @brief Checks the overload flag bit.
+    /// @return True if the overload flag bit is set, false otherwise
+    bool getOverloadFlag();
+
+    /// @brief Checks the conversion ready flag bit.
+    /// @return True if that flag bit is set, false otherwise
+    bool getConvReadyFlag();
+
+    /// @brief Checks the too bright flag bit.
+    /// @return True if that flag bit is set, false otherwise
+    bool getTooBrightFlag();
+
+    /// @brief Checks the too dim flag bit.
+    /// @return True if that flag bit is set, false otherwise
+    bool getTooDimFlag();
+    
     ///////////////////////////////////////////////////////////////////Color Information
+    /// @brief Reads Channel Zero (Red)
+    /// @return Returns the ADC value of Channel Zero
     uint32_t getADCCh0();
+
+    /// @brief Reads Channel One (Green)
+    /// @return Returns the ADC value of Channel One
     uint32_t getADCCh1();
+
+    /// @brief Reads Channel Two (Blue)
+    /// @return Returns the ADC value of Channel Two
     uint32_t getADCCh2();
+
+    /// @brief Reads Channel Three (White)
+    /// @return Returns the ADC value of Channel Three
     uint32_t getADCCh3();
+
+    /// @brief Retrieves all ADC values for all channels: Red, Green, Blue, and White.
+    /// @return Returns the ADC values of the channels.
     sfe_color_t getAllADC();
+
+    /// @brief Retrieves all ADC values for all channels: Red, Green, Blue, and White, as well as the sample counter, and
+    ///        the CRC value.
+    /// @param color Pointer to the color struct to be populated with the channels values.
+    /// @return Returns true on successful execution, false otherwise.
     bool getAllChannelData(sfe_color_t *color);
+
+    /// @brief  Calculates the CRC for the OPT4048. Note that the OPT4048 does this already
+    ///         but this is a way to double check the value.
+    /// @param mantissa The mantissa value of the ADC
+    /// @param exponent The exponent value of the ADC
+    /// @param crc The CRC value of the ADC
+    /// @return Returns the calculated CRC value.
     uint8_t calculateCRC(uint32_t manitssa, uint8_t expon, uint8_t crc);
 
+    /// @brief Retrieves the Lux value.
+    /// @return Returns the Lux value of the sensor
     uint32_t getLux();
+
+    /// @brief  Retrieves the CIE X value of the sensor.
+    /// @return Returns the CIE X value of the sensor
     double getCIEx();
+
+    /// @brief Retrieves the CIE Y value of the sensor.
+    /// @return Returns the CIE Y value of the sensor
     double getCIEy();
+
+    /// @brief Retrieves the Correlated Color Temperature (CCT) of the sensor.
+    /// @return Returns the CCT of the sensor in Kelvin
     double getCCT();
 
   private:
